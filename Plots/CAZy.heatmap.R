ef39e22f07f6cc56cb8a65b2cf3ce8ece7b3d033
Rafael <rafael.lopez@ibt.unam.mx>
2021-03-19 10:12:40 -0600

    update

--- Plots/CAZy.heatmap.R
+++ Plots/CAZy.heatmap.R
@@ -13,16 +13,18 @@ library(vegan)
 library(gplots)
 library(RColorBrewer)
 library(Heatplus)
+library(viridis)
+
 ##Heatmap
 cazymes <- read.table('cazy_abund.txt', header=TRUE, row.names=1 ,sep ="\t")
 #2. Define color palette and row side colors palette for the 3 categories.
 my_pallet <- colorRampPalette(c('blue','red'))(n = 299)
-var1 <- c(rep("gold", 14),rep("green4", 38),rep("darkorange", 2), rep("dodgerblue", 2))
+var1 <- c(rep("#fbb4ae", 14),rep("#b3cde3", 38),rep("#ccebc5", 2), rep("#decbe4", 2))
 #4. Define data characteristics
-col_breaks = c(seq(-0,1.09, length=100), seq(1.1,2.09,length=100), seq(2.1,8,length=100))
+col_breaks = c(0:2, seq(from = 40, to = max(cazymes) + 10, by = 10))
 #5. Draw heatmap
 png("CAZy_heatmap.png", width=1850, height=1850, res = 400)
-heatmap.2(as.matrix(cazymes), col = my_pallet, RowSideColors = var1,  main = "CAZymes", density.info = 'none', trace = 'none', dendrogram = 'col', Rowv = "NA", cexCol = 0.62, cexRow = 0.49, margins = c(5,12), tracecol = "both", breaks = col_breaks,colsep=1:nrow(cazymes),rowsep=1:nrow(cazymes),sepcolor = "black", sepwidth=c(0.0001, 0.0001), offsetRow=-0.3, offsetCol=-0.3, key.title=TRUE, key.xlab="CAZy abundance")
+heatmap.2(as.matrix(cazymes), col = viridis::viridis_pal(), RowSideColors = var1,  main = "CAZymes", density.info = 'none', trace = 'none', dendrogram = 'col', Rowv = "NA", cexCol = 0.62, cexRow = 0.49, margins = c(5,14), tracecol = "both", breaks =col_breaks,colsep=1:nrow(cazymes),rowsep=1:nrow(cazymes),sepcolor = "black", sepwidth=c(0.0001, 0.0001), offsetRow=-0.3, offsetCol=-0.3, key.title=TRUE, key.xlab="CAZy abundance(%)")
 ##Add legend to rowSide
-legend("bottomleft", xpd = TRUE, legend = c("Starch","PCW","Sucrose", "Fructan"), col= c("gold", "green4","darkorange","dodgerblue"), lty = 1,lwd = 4, cex = 0.30)
+legend("bottomleft", xpd = TRUE, legend = c("Starch","PCW","Sucrose", "Fructan"), col= c("#fbb4ae", "#b3cde3","#ccebc5","#decbe4"), lty = 1,lwd = 4, cex = 0.30)
 dev.off()
