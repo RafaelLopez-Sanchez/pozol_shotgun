adf74df7135048da0291c0ce5b329798fee7af67
Rafael <rafael.lopez@ibt.unam.mx>
2021-10-22 13:02:54 -0500

    Update CAZy.heatmap.R

--- Plots/CAZy.heatmap.R
+++ Plots/CAZy.heatmap.R
@@ -23,8 +23,35 @@ var1 <- c(rep("#fbb4ae", 14),rep("#b3cde3", 38),rep("#ccebc5", 2), rep("#decbe4"
 #4. Define data characteristics
 col_breaks = c(0:2, seq(from = 40, to = max(cazymes) + 10, by = 10))
 #5. Draw heatmap
-png("CAZy_heatmap.png", width=1850, height=1850, res = 400)
-heatmap.2(as.matrix(cazymes), col = viridis::viridis_pal(), RowSideColors = var1,  main = "CAZymes", density.info = 'none', trace = 'none', dendrogram = 'col', Rowv = "NA", cexCol = 0.62, cexRow = 0.49, margins = c(5,14), tracecol = "both", breaks =col_breaks,colsep=1:nrow(cazymes),rowsep=1:nrow(cazymes),sepcolor = "black", sepwidth=c(0.0001, 0.0001), offsetRow=-0.3, offsetCol=-0.3, key.title=TRUE, key.xlab="CAZy abundance(%)")
+tiff("CAZy_heatmap.tiff",  units="cm", width=8.5, height=10.5, res=300)
+heatmap.2(as.matrix(cazymes), 
+	col = viridis::viridis_pal(), 
+	RowSideColors = var1,  
+	cex.main = 1.5,	
+	main = "CAZymes", 
+	density.info = 'none', 
+	trace = 'none', 
+	dendrogram = 'col', 
+	Rowv = "NA", 
+	cexCol = 0.4, 
+	cexRow = 0.4, 
+	margins = c(6,10), 
+	tracecol = "both", 
+	breaks =col_breaks,
+	colsep=1:nrow(cazymes),
+	rowsep=1:nrow(cazymes),
+	sepcolor = "black", 
+	sepwidth=c(0.0001, 0.0001),
+	offsetRow=-0.2,
+	offsetCol=-0.2, 
+	key.title=TRUE, 
+	key.xlab="CAZy abundance(%)",
+	key.par = list(cex=0.6))
 ##Add legend to rowSide
-legend("bottomleft", xpd = TRUE, legend = c("Starch","PCW","Sucrose", "Fructan"), col= c("#fbb4ae", "#b3cde3","#ccebc5","#decbe4"), lty = 1,lwd = 4, cex = 0.30)
+legend("bottomright", xpd = TRUE,
+	title="SUBSTRATE", 
+	text.font =2 , 
+	legend = c("Starch","PCW","Sucrose", "Fructan"),
+	col= c("#fbb4ae", "#b3cde3","#ccebc5","#decbe4"), 
+	lty = 1,lwd = 4, cex = 0.30)
 dev.off()
